6763f50239d1e0b6eae9004a80827962e8846426
toma222 <aidannwilhite@gmail.com>
2023-11-26 15:35:56 +0000

    made a super cool premake file

---
+++ premake5.lua
@@ -0,0 +1,24 @@
+
+project "lua"
+	kind "StaticLib"
+	language "C"
+
+	targetdir ("%{wks.location}/bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("%{wks.location}/bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"**.h",
+		"**.c"
+	}
+
+	filter "configurations:Debug"
+		defines "DEBUG"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		defines "NDEBUG"
+		runtime "Release"
+		optimize "on"
+
